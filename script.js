8e4284af7cd1d0dffd97155e89d39de694c1599a
Andrelon de Souza <dedeh.souza2019@gmail.com>
2025-07-17 16:34:44 -0300

    Desafio realizado aplicando ferramentes como classe, objetos, estruturas condicionais, entre outras ferramentas aprendidas durante o curso de lógica de programação.

---
+++ script.js
@@ -0,0 +1,29 @@
+class Heroi {
+    constructor(nomeHeroi, idadeHeroi, tipoHeroi) {
+        this.nomeHeroi = nomeHeroi;
+        this.idadeHeroi = idadeHeroi;
+        this.tipoHeroi = tipoHeroi;
+    }
+
+    atacar() {
+        let ataque = "";
+
+        if(this.tipoHeroi === "Mago") {
+            ataque = "Magia";
+        } else if(this.tipoHeroi === "Guerreiro") {
+            ataque = "Espada";
+        } else if(this.tipoHeroi === "Monge") {
+            ataque = "Artes Marciais";
+        } else {
+            ataque = "Shuriken";
+        }
+
+       console.log(`O ${this.tipoHeroi} atacou usando ${ataque}!`);
+    }
+}
+
+let superMan = new Heroi("Super-man", 23, "Mago");
+let robby = new Heroi("Robby", 25, "Monge");
+
+superMan.atacar();
+robby.atacar();
